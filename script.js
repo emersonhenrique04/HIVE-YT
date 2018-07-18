49a8eacd25372b1fdff3860a28fd56411962816a
Victor Yukio <victorkikio@gmail.com>
2018-07-18 16:46:51 -0300

    atualização menu respon

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 //CARROSSEL
+const menu = document.querySelector("aside");
+const botao = document.querySelector("#hamburger");
+
+function deslizar(){
+    if(menu.classList.contains("active")){
+        menu.classList.remove("active");
+        botao.classList.remove("active");
+    }
+    else{
+        menu.classList.add("active");
+        botao.classList.add("active");
+    }
+    
+}
+
+botao.onclick = deslizar;
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
